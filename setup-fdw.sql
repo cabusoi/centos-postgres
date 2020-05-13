dcbaa38caf6670273b83eb05d38b3d916a3fce53
c.busoi <c.busoi@ca>
2020-05-13 08:02:51 +0200

    fdw extension

--- setup-fdw.sql
+++ setup-fdw.sql
@@ -3,4 +3,3 @@ CREATE SCHEMA import_schema;
 CREATE SERVER sql01 FOREIGN DATA WRAPPER tds_fdw OPTIONS (servername 'mssql', database 'master', msg_handler 'notice');
 CREATE USER MAPPING FOR postgres SERVER sql01 OPTIONS (username 'sa', password 'P4ssw0rD');
 IMPORT FOREIGN SCHEMA msschema01 FROM SERVER sql01 INTO import_schema OPTIONS (import_default 'true');
-
